dd248fb43c134faef2208117860128798ee07d3f
Ad3an <144132964+Ad3an@users.noreply.github.com>
2024-03-10 15:54:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+gsap.from(".nav , .set",{
+    stagger:0.3,
+    y:13,
+    opacity:0,
+    duration:1
+
+})
+gsap.from(".trending-section-right img",{
+    x:40,
+    duration:2,
+    opacity:0
+})
+gsap.from(".trending-section",{
+    opacity:0,
+    duration:3
+})
